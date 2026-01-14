c4e51d3e7c5e0caa0ec480b8900ed1293367430a
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-01-14 11:42:35 +0300

    sets up db

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphqlModule } from './modules/graphql/graphql.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
   imports: [
-    GraphqlModule
+    GraphqlModule,
+    DatabaseModule,
   ],
 })
 export class AppModule {}
