ec3dbacf712e2ecc2746fb242b431a49e0da0da2
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-01-17 08:11:17 +0300

    UI fixes

--- libs/admin/pages/contributions/src/lib/pages/index.ts
+++ libs/admin/pages/contributions/src/lib/pages/index.ts
@@ -2,3 +2,4 @@ export * from './dashboard/contribution-dashboard.component';
 export * from './list/contribution-list.component';
 export * from './form/contribution-form.component';
 export * from './detail/contribution-detail.component';
+export * from './pending/pending-contributions.component';
