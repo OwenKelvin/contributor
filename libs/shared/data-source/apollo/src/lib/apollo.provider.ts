7073fcad164db14c048668e5db73d9823c11406e
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-01-17 07:56:57 +0300

    UI fixes

--- libs/shared/data-source/apollo/src/lib/apollo.provider.ts
+++ libs/shared/data-source/apollo/src/lib/apollo.provider.ts
@@ -8,6 +8,7 @@ import { createClient } from 'graphql-ws';
 
 import { contextSuccessAlert } from './success-alert.context';
 import { BACKEND_URL } from '@nyots/data-source/constants';
+import { contextAuthToken } from './auth-token.context';
 
 export const apolloConfig = () => {
   const httpLink = inject(HttpLink)
@@ -41,6 +42,7 @@ export const apolloConfig = () => {
 
   const link = ApolloLink.from([
     contextSuccessAlert(),
+    contextAuthToken(),
     splitLink,
   ]);
 
