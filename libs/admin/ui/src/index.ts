01507bb214872b87360be3b2e63eb8c088d40dd0
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-01-21 15:06:47 +0300

    Bulk actions

---
+++ libs/admin/ui/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/dialogs/payment-processing/payment-processing-dialog.component';
+export * from './lib/dialogs/refund-reason/refund-reason-dialog.component';
+export * from './lib/dialogs/status-update/status-update-dialog.component';
+export * from './lib/dialogs/bulk-status/bulk-status-dialog.component';
+export * from './lib/dialogs/bulk-assign-role/bulk-assign-role-dialog.component';
+export * from './lib/dialogs/bulk-ban-users/bulk-ban-users-dialog.component';
