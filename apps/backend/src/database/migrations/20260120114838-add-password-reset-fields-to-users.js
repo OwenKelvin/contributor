a0591961e7d211d33d1705150f4876c032c4b9b4
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-01-21 09:42:10 +0300

    Password Reset

--- apps/backend/src/database/migrations/20260120114838-add-password-reset-fields-to-users.js
+++ apps/backend/src/database/migrations/20260120114838-add-password-reset-fields-to-users.js
@@ -3,20 +3,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('users', 'password_reset_token', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('users', 'password_reset_expires', {
+      type: Sequelize.DATE,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('users', 'password_reset_token_used', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('users', 'password_reset_token');
+    await queryInterface.removeColumn('users', 'password_reset_expires');
+    await queryInterface.removeColumn('users', 'password_reset_token_used');
   }
 };
