b94490d26b76889323fedd6019e22ad4cb2fdf82
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-01-22 18:51:23 +0300

    System health

---
+++ libs/admin/pages/system-health/src/lib/system-health.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { SystemHealth } from './system-health';
+
+export const SYSTEM_HEALTH_ROUTES: Route[] = [
+  { path: '', component: SystemHealth },
+];
