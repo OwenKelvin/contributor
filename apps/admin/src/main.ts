6cb5bba87afba3dab6102b1ba9a431c7548b543f
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2026-04-17 16:24:41 +0300

    FIXES

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -2,4 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+console.log('Application started');
+
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
